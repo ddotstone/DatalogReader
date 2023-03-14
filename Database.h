8eaa6f1aeb87b7aec99c623cc237d0c96551256e
Todd Johnston <johnstontoddc@gmail.com>
2023-03-14 01:37:48 -0600

    Last Changes

--- Database.h
+++ Database.h
@@ -8,7 +8,7 @@
 class Database {
 private:
 	std::map<std::string,Relation> relations;
-	std::vector<Predicate*> queries;
+	std::vector<Relation> queries;
 public:
 	std::string toString() {
 		std::stringstream s;
@@ -34,13 +34,60 @@ public:
 		
 	}
 	
-	void processQueries(DatalogProgram& datalog) {
-		std::vector<int> indexes = {0};
-		std::vector<std::string> values = {"'b'","X"};
-		Relation temp = this->relations["SK"].select(indexes,values);
-		std::cout << temp.toString() << "\n";
-
-	}
+	void processQueries(DatalogProgram datalog) {
+		std::vector<std::string> values;
+		std::vector<int> indexes;
+		std::vector<int> var_indexes;
+		std::set<std::string> vars;
+		unsigned int k;
+		Relation temp;
+		Relation temp2;
+		for (auto& query : datalog.getQueries()) {
+			values = conToString(query.getParameters());
+			indexes.clear();
+			for (unsigned int i = 0; i < values.size(); i++){
+				if (values.at(i)[0] == '\'') {
+					indexes.push_back(i);
+				}
+			}
+			temp = relations[query.getName()].select(indexes, values);
+			k = 0;
+			var_indexes.clear();
+			vars.clear();
+			for (unsigned int i = 0; i < values.size(); i++) {
+				if (k < indexes.size() && (int)i == indexes.at(k)) {
+					k++;
+					continue;
+				}
+				if (vars.find(values.at(i)) == vars.end()) {
+					var_indexes.push_back(i);
+					vars.insert(values.at(i));
+				}
+			}
+			temp2 = temp.project(var_indexes);
+			std::vector<std::string> variables;
+			for (auto& val : var_indexes) {
+				variables.push_back(values.at(val));
+				
+			}
+			var_indexes.clear();
+			for (unsigned int i=0; i < variables.size();i++) {
+				var_indexes.push_back(i);
+			}
+			temp2 = temp2.rename(var_indexes, variables);
+			std::cout << query.toString() << "? ";
+			if (temp.numTuples() > 0) {
+				std::cout << "Yes(" << temp.numTuples() << ")" << "\n";
+				if (temp2.getScheme().size() != 0) {
+					std::cout << temp2.toString() << "\n";
+					}
+			}
+			else {
+				std::cout << "No" << "\n";
+			}
+			this->queries.push_back(temp);
+		}
+}
 	std::vector<std::string> conToString(std::vector<Parameter> values) const{
 		std::vector<std::string> temp;
 		for (auto& i : values) {
--- Relation.h
+++ Relation.h
@@ -11,113 +11,150 @@ private:
   Scheme scheme;
   long unsigned int scheme_len;
   std::set<Tuple> tuples;
+  int num_tuples;
 
 public:
-	Relation() {};
-  Relation(const std::string &name, const Scheme &scheme)
-      : name(name), scheme(scheme), scheme_len(scheme.size()) {}
-  bool addTuple(const Tuple &tuple) {
-    if (tuple.size() == scheme_len) {
-      tuples.insert(tuple);
-      return true;
-    }
-    return false;
-  }
-std::string toString() const {
-	std::stringstream s;
-	for (auto& tuple:tuples){
-		s<<tuple.toString(scheme)<<"\n";
+	
+	
+	Relation() {
+		num_tuples = 0;
+	};
+  
+	
+	Relation(const std::string& name, const Scheme& scheme)
+		: name(name), scheme(scheme), scheme_len(scheme.size()) {
+		num_tuples = 0;
 	}
-	return s.str();
-}
-Relation select(const std::vector <int>& indexes, const std::vector<std::string>& values) const {
-	Relation result(this->name,this->scheme);
-	bool is_match;
-	unsigned int j = 0;
-	unsigned int k = 0;
-	unsigned int l = 0;
-	std::unordered_map <int, std::vector<int>> ref_;
-	std::set<std::string> found;
-	for (unsigned int i = 0; i < values.size(); ++i) {
-		if ((int)i == (int)indexes.at(l)) {
-			l++;
-			continue;
+  
+  
+	bool addTuple(const Tuple &tuple) {
+		if (tuple.size() == scheme_len) {
+		  tuples.insert(tuple);
+		  num_tuples++;
+		  return true;
 		}
-		k = 0;
-		if (found.find(values.at(i)) != found.end()) {
-			found.insert(values.at(i));
-			k = i;
-			while (k < values.size()) {
-				if (values.at(i) == values.at(k)) {
-					ref_[i].push_back(k);
-				}
-				k++;
-			}
-		}
+		return false;
+	  }
+
+	int numTuples() {
+		return this->num_tuples;
 	}
-	for (auto&tuple:tuples){
-		is_match = true;
-		j = 0;
-		for (unsigned int i = 0; i < values.size(); i++){
-			if ((int)i == (int)indexes.at(j)){
-				if (tuple.at(i) != values.at(i)) {
-				is_match = false;
-				break;
+
+
+	std::string toString() const {
+		std::stringstream s;
+		for (auto& tuple:tuples){
+			s<<"  "<<tuple.toString(scheme) << "\n";
+		}
+		s.seekp(-1, s.cur); 
+		s << '\0';
+		return s.str();
+	}
+
+
+	Relation select(const std::vector <int>& indexes, const std::vector<std::string>& values) const {
+		Relation result(this->name,this->scheme);
+		bool is_match;
+		unsigned int j = 0;
+		unsigned int k = 0;
+		unsigned int l = 0;
+		std::unordered_map <int, std::vector<int>> ref_;
+		std::set<std::string> found;
+		for (unsigned int i = 0; i < values.size(); ++i) {
+			if (l < indexes.size()) {
+				if ((int)i == (int)indexes.at(l)) {
+					l++;
+					continue;
 				}
-				j++;
 			}
-			else {
-				if (ref_.find(i)!= ref_.end() && !ref_[i].empty())
-					for (auto& pos : ref_[i]) {
-						if (tuple.at(pos) != tuple.at(i)) {
-							is_match = false;
-							break;
-						}
+			k = 0;
+			if (found.find(values.at(i)) != found.end()) {
+				found.insert(values.at(i));
+				k = i+1;
+				while (k < values.size()) {
+					if (values.at(i) == values.at(k)) {
+						ref_[i].push_back(k);
 					}
+					k++;
+				}
 			}
 		}
-		if (is_match) {
-			result.addTuple(tuple);
+		for (auto&tuple:tuples){
+			is_match = true;
+			j = 0;
+			for (unsigned int i = 0; i < values.size(); i++){
+				if (j < indexes.size()) {
+					if ((int)i == (int)indexes.at(j)) {
+						if (tuple.at(i) != values.at(i)) {
+							is_match = false;
+								break;
+						}
+						j++;
+					}
+				}
+				else {
+					if (ref_.find(i)!= ref_.end() && !ref_[i].empty())
+						for (auto& pos : ref_[i]) {
+							if (tuple.at(pos) != tuple.at(i)) {
+								is_match = false;
+								break;
+							}
+						}
+				}
+			}
+			if (is_match) {
+				result.addTuple(tuple);
+			}
 		}
+		return result;
 	}
-	return result;
-}
-Relation project(const std::vector<int>& indexes) const{
-	Scheme temp1;
-	Tuple temp2;
-	std::map<int, bool> ref;
-	for (auto& index:indexes) {
-		temp1.push_back(this->scheme.at(index));
+
+
+	Relation project(const std::vector<int>& indexes) const{
+		Scheme temp1;
+		Tuple temp2;
+		std::map<int, bool> ref;
+		for (auto& index:indexes) {
+			temp1.push_back(this->scheme.at(index));
+		}
+		Relation result(this->name,temp1);
+		for (auto& tuple: tuples) {
+			temp2 = {};
+			for (auto& val : indexes) {
+				temp2.push_back(tuple.at(val));
+				result.addTuple(temp2);
+			}
+		}
+		return result;
 	}
-	Relation result(this->name,temp1);
-	for (auto& tuple: tuples) {
-		temp2 = {};
+
+
+	Relation rename(std::vector<int>& indexes, std::vector<std::string>& values) {
+		Scheme scheme = this->scheme;
 		for (auto& val : indexes) {
-			temp2.push_back(tuple.at(val));
-			result.addTuple(temp2);
+			scheme.at(val) = values.at(val);
 		}
+		Relation result = *this;
+		result.changeScheme(scheme);
+		return result;
 	}
-	return result;
-}
-Relation rename(std::vector<int>& indexes, std::vector<std::string>& values) {
-	Scheme scheme = this->scheme;
-	for (auto& val : indexes) {
-		scheme.at(val) = values.at(val);
+
+
+	void changeScheme(Scheme scheme) {
+		this->scheme = scheme;
 	}
-	Relation result = *this;
-	result.changeScheme(scheme);
-	return result;
-}
-
-void changeScheme(Scheme scheme) {
-	this->scheme = scheme;
-}
+	
+	Scheme getScheme() {
+		return this->scheme;
+	}
 
 
-void setName(std::string name) {
-	this->name = name;
-}
-std::string getName() const{
-	return this->name;
-}
-};
+	void setName(std::string name) {
+		this->name = name;
+	}
+
+
+	std::string getName() const{
+		return this->name;
+	}
+	};
